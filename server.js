8c16b9f1d39578856a191132718136da7a0fdaa2
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2025-03-25 14:37:03 +0530

    Add port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("server is running in port 3000");
 });
